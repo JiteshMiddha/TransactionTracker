f7026b50668065288dc9c1d55e68ed06439f0c13
Jitesh Middha <jiteshmiddha@ymail.com>
2018-07-06 01:34:26 +0530

    Filters applied

--- app/controllers/api/v1/transaction_controller.rb
+++ app/controllers/api/v1/transaction_controller.rb
@@ -14,21 +14,46 @@ module Api
 			# end
 
 
-			def create
-
-				
+			def create				
 				# transaction = TransactionDatum.new(transaction_params
 
 				transactionDetails = TransactionRequest.new(:from_service_name => params[:from_service_name], :request_type => params[:data][:type], :created_at => params[:data][:data][:created_at], :user_email => params[:data][:data][:user_email], :inc_id => params[:data][:data][:inc_id], :transaction_id => params[:data][:data][:transaction_id], :amount => params[:data][:data][:amount], :action => params[:data][:data][:action])
 				if transactionDetails.save
-					render json: {status: 'success', details: transactionDetails},status: :ok
+
+					text = ''
+					if check1hour > 3 
+						text = "3 transactions in 1 hour limit crossed"
+					end
+					if checkTotalAmountIn24Hours > 10000 
+						text = text + "\nDebit Amount in 24 hours crossed 10,000"
+					end
+					
+
+
+					render json: {status: 'success', details: text},status: :ok
 				else
 					render json: {status: 'error', message: 'transaction error'},status: :unprocessable_entry
 				end
 			end
 
 
-			private
+			def check1hour
+
+				transactions = TransactionRequest.where(updated_at: (Time.now - 1.hours)..Time.now).where(:user_email => params[:data][:data][:user_email])
+				return transactions.count
+			end
+
+			def checkTotalAmountIn24Hours
+
+				total = TransactionRequest.where(updated_at: (Time.now - 24.hours)..Time.now).where(:user_email => params[:data][:data][:user_email]).sum(:amount)
+				return total
+			end
+
+			def sendMail(text)
+
+
+			end
+			# private
 			# def transaction_params
 			# 	params.permit(:transaction_type, :transactionId, :fromServiceName)
 			# end
